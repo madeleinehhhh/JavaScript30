73446529ee89ba59f5324b18ae5392e56b34863b
madeleine <mhs.moogy@gmail.com>
2021-08-20 17:27:06 -0500

    moved, whoops

---
+++ 01-JavaScriptDrumKit/01-javascriptdrumkit.js
@@ -0,0 +1,7 @@
+const keys = document.querySelectorAll('.key')
+
+keys.forEach((key)) => {
+  key.addEventListener('click', () => {
+    //on clicking a key, we want to add the class .playing
+  })
+}
